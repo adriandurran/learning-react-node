f7d1e557e2c4b3557ada30998f8ab585befff4a9
Adrian <adrian@intellidroid.eu>
2018-02-08 20:17:19 +0000

    added mailer

--- server/routes/survey_routes.js
+++ server/routes/survey_routes.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Mailer = require('../services/mailer');
+const surveyTemplate = require('../services/email_templates/surveyTemplate');
+
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
       title,
@@ -16,5 +19,13 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+    } catch (err) {
+      console.log(err);
+      res.status(422).send(err);
+    }
   });
 };
