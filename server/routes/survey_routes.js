3f8c20bf0cef0d53ee06bd13e96d5ced089552ca
Adrian <adrian@intellidroid.eu>
2018-02-11 10:26:06 +0000

    finished off route

--- server/routes/survey_routes.js
+++ server/routes/survey_routes.js
@@ -19,10 +19,15 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
     const mailer = new Mailer(survey, surveyTemplate(survey));
 
     try {
       await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
     } catch (err) {
       console.log(err);
       res.status(422).send(err);
